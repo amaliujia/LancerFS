cd8320d9f95d33c849fd4ca6a7cfa70152ab1a9c
amaliujia <amaliujia@163.com>
2015-04-10 14:52:08 -0400

    add test script

---
+++ src/scripts/total.sh
@@ -0,0 +1,7 @@
+./test_part1.sh small_test.tar.gz
+./test_part1.sh	big_test.tar.gz
+./test_part1.sh	large_test.tar.gz
+./test_part2.sh small_test.tar.gz
+./test_part2.sh	big_test.tar.gz
+./test_part2.sh	large_test.tar.gz
+ 
